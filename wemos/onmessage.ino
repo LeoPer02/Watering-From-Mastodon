49805ebefa7adf44a455c6652d7408756c890121
João Azevedo <62391524+Azeved00@users.noreply.github.com>
2024-06-10 22:03:55 +0100

    wemos: added functions

--- wemos/onmessage.ino
+++ wemos/onmessage.ino
@@ -1,3 +1,33 @@
+#DEFINE MAX_CHAR
+
+int mqtt_read_int(){
+  char str[10];  
+  int acumulator = 0;
+  while (true) {
+    char ch = mqttCleint.peek();
+    if (ch == '\n' || ch == ' '){
+      break;
+    }
+    acumulator *=10;
+    char c = mqttClient.read();
+    acumulator += c - '0';
+  }
+  return;
+}
+
+int mqtt_clean(){
+  while (true) {
+    char ch = mqttClient.read();
+
+    if (ch == '\n'){
+      break;
+    }
+  }
+  return;
+}
+
+
+
 void onMqttMessage(int messageSize) {
 
   // we received a message, print out the topic and contents
@@ -8,9 +38,15 @@ void onMqttMessage(int messageSize) {
   Serial.println(" bytes:");
 
 
-  // use the Stream interface to print the contents
-  while (mqttClient.available()) {
-    Serial.print((char)mqttClient.read());
+  if(mqttClient.messageTopic() == TOPIC_THRESHOLD){
+
+  }
+  else{
+    int id = mqtt_read_int();
+    if(id != MQTT_ID){
+      mqtt_clean();
+    }
+
   }
 
   Serial.println();
