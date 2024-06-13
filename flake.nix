8308996e5a138e7530b95ffa549faeef366ad6e2
João Azevedo <62391524+Azeved00@users.noreply.github.com>
2024-06-13 08:38:00 +0100

    added docker and mqtt-explorer to flake.nix

--- flake.nix
+++ flake.nix
@@ -11,11 +11,24 @@
         ROOT = let p = builtins.getEnv "PWD"; in if p == "" then self else p;
         name = "Arduino";
         system = "x86_64-linux";
+        mqtt-explorer= pkgs.symlinkJoin {
+            name = "mqtt-explorer";
+            paths = [ pkgs.appimage-run ];
+            buildInputs = [ pkgs.makeWrapper ];
+            postBuild = ''
+                mv $out/bin/appimage-run $out/bin/mqtt-explorer
+                wrapProgram $out/bin/mqtt-explorer --add-flags "./mqtt-explorer.AppImage"
+            '';
+        };
     in {
         devShells."${system}".default = pkgs.mkShell {
             inherit name ROOT;
 
-            buildInputs = with pkgs; [arduino-ide python3];
+            buildInputs = with pkgs; [
+                arduino-ide python3
+                docker
+                mqtt-explorer
+            ];
 
             shellHook = '''';
 
